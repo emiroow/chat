4f4a1bd53a50255631d38106d2161d4c7b2cf417
emiroow <amir_esmaeelzadeh@yahoo.com>
2025-11-10 20:26:14 +0330

    refactor: remove navigation logic from ChatsPage component

--- client-signalR/src/pages/ChatsPage.tsx
+++ client-signalR/src/pages/ChatsPage.tsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import { Sidebar } from "../components/chat/Sidebar";
 
 export const ChatsPage: React.FC = () => {
-  const navigate = useNavigate();
-
   return (
     <div className="flex h-dvh w-full bg-(--bg) text-(--text)">
-      <Sidebar onSelect={(id) => navigate(`/${id}`)} />
+      <Sidebar />
     </div>
   );
 };
